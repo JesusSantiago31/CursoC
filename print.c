7f550b087c8fd2d14d93cbf5eb6d550ea28d369f
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-13 02:10:46 -0600

    Add files via upload
    First part of the curse, where i learned how to print in console the famous word "Hello world"

---
+++ print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    printf("Este es un texto \n");    
+    printf("que fue impreso desde c.");
+    printf("\n");
+    printf("Ya soy todo un programador ");
+    printf("\n");
+    printf("\a");//sonidito
+
+    return 0;
+}
