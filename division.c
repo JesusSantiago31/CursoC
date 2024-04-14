3323e3acc3169b4663aef0f80c5b16c52f6c5e8e
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-14 02:32:45 -0600

    Add files via upload

---
+++ division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    double var1 = 10;
+    double var2 = 5;
+    double res = var1/var2;
+    printf("El resultado de dividir %f / %f es; %f", var1, var2, res);
+    
+    return 0;
+
+}
