80c4477c4b36edf4c559a03744489a9eac2916f8
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-15 20:52:06 -0600

    Add files via upload
    Funcionamiento de los operadores de preincremento y predecremeto y sus diferencias

---
+++ preincremento.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int var = 1;
+    int con = 1;
+
+    printf(" %d",++var);
+    printf("\n %d",var);
+
+    printf("\n %d",--con);
+    printf("\n %d",con);
+
+    printf("\n");
+    return 0;
+
+}
