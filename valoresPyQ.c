d70a080e03e1381d2e670a4c92212e9d6c44879e
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-16 23:13:18 -0600

    Add files via upload
    Algunas operaciones realizadas con los operadores logicos y relacionales, inicializando algunos valores con true and false para observar su comportamientos

---
+++ valoresPyQ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int P = 1;
+    int Q = 0;
+    int R = 1;
+    int T = 0;
+    
+    printf("P & R = %d \n", (P&R));
+    printf("Q | T = %d \n", (Q | T));
+    printf("((P & Q) |  (R & T)) = %d \n", ((P & Q) |  (R & T)));
+    printf("((P ^ Q) ^ ( R ^ T)) = %d \n", ((P ^ Q) ^ ( R ^ T)) );
+    printf("((Q!) & (T!)) = %d \n", ((!Q) & (!T)));
+    printf("(!(!(!P))) = %d \n", (!(!(!P))));
+
+
+}
