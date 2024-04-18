b4d1a15b6051589f2aa49b9020bd4832848b73bc
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-17 20:16:40 -0600

    Add files via upload
    Comparacion de las palabras reservadas puts
    y printf\n
    .

---
+++ putsPrintf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    int x; 
+    
+    printf("Hola mundo \n");
+    puts("Ingresa un valor");
+    scanf("%d", &x);
+    printf("x = %d ", x);
+    return 0; 
+}
