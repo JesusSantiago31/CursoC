624959b13a155916073612de79f232ceef55812c
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-19 20:17:02 -0600

    Add files via upload
    Explicacion basica de como es que funciona un arreglo

---
+++ primerArr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int miArreglito[10];
+
+    printf("vector[0] = %d \n",( miArreglito[0] = 5));
+    printf("vector[1] = %d \n", (miArreglito[1] = 10));
+    printf("vector[2] = %d \n", (miArreglito[2] = 10));
+    printf("vector[3] = %d \n", (miArreglito[3] = 10));
+    printf("vector[4] = %d \n", (miArreglito[4] = 10));
+    printf("vector[5] = %d \n", (miArreglito[5] = 10));
+    printf("vector[6] = %d \n", (miArreglito[6] = 10));
+    printf("vector[7] = %d \n", (miArreglito[7] = 10));
+    printf("vector[8] = %d \n", (miArreglito[8] = 10));
+    printf("vector[9] = %d \n", (miArreglito[9] = 10));
+    
+    return 0;
+}
