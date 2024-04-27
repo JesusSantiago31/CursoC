fd36015caed837059625cc7c106b758d8e889ae9
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-04-26 23:20:48 -0600

    Add files via upload

---
+++ apuntadores.c
@@ -0,0 +1,10 @@
+//tipo de dato * identificador del apuntador
+//referenciacion &x
+#include <stdio.h>
+int main(){
+    int a = 20;
+    printf("El valor de a es: %d\n", a);
+    printf("La direccion de memroia es: %p\n",&a);
+
+    return 0;
+}
