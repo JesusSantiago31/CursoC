9f49d411b9617568f2f9f3ede234ee9bb4c74580
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-05-02 23:40:24 -0600

    Add files via upload

--- arreglo.c
+++ arreglo.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
-    int vector[10];
-    int i = 0;
-    while(i<10){
-        vector[i]=10;        
-        i++;    
+    int i,n;
+    int *buffer;
+    printf("Teclea la logitud de la cadena ");
+    scanf("%d",&i);
+
+    buffer = (int*) malloc((i+1)*sizeof(int));
+    if (buffer==NULL) exit(1);
+
+    for(n=0; n<i ; n++){
+        printf("Ingresa el valor en la posicion %d ", n);
+        scanf("%d", &buffer[n]);
+        printf("\n");
+        
     }
-    i=0;
-    while(i<10){        
-        printf("El arreglo en la posicion [%d] es = %d \n", i, vector[i]);
-        i++;    
-    }
+    for(n=0; n<i ; n++){
+        printf("Buffer [%d]: %d\n", n,  buffer[n]);
+        
+    }   
+        
+    buffer[i]='\0';        
+    free(buffer);
+
     return 0;
 }
